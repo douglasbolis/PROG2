fc9b0edc10fe651223147e163cc8db6ebc09caac
douglasbolis <doulgas.lima@xdevel.com.br>
2015-04-07 19:55:47 -0300

    Primeira apresentação das atividades em prog2.

---
+++ atividade_1/exerc_1.py
@@ -0,0 +1,17 @@
+__author__ = 'douglas'
+
+def separaPalavras(texto):
+    a = 0
+#fim funcao
+
+def main():
+    texto = "Construa a função separaPal(<text>) que recebe como entrada um texto contendo um documento qualquer. " \
+            "A Função deve retornar uma lista contendo as palavras presente no texto. " \
+            "Uma palavra é uma sequencia de caracteres entre caracteres chamados de separadores.";
+
+    separaPalavras(texto)
+#fim main
+
+if __name__ == "__main__":
+    main()
+#fim if
