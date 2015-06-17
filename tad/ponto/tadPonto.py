702b9257513188e6a08a6147e8bdc37cd5d29c9f
douglasbolis <douglas.lima@xdevel.com.br>
2015-06-17 14:56:53 -0300

    Ajustado algumas função do tadPonto.

--- tad/ponto/tadPonto.py
+++ tad/ponto/tadPonto.py
@@ -13,19 +13,29 @@ def distancia(paramTADptoA, paramTADptoB):
 
 def getXY(paramTADpto):
     """Retorna uma tupla com as coordenadas x e y do ponto."""
+    return (paramTADpto[0], paramTADpto[1])
 # fim função
 
 def igual(paramTADptoA, paramTADptoB):
     """Retorna boolean True se os dois tad ponto de entrada forem iguais
     (mesmo valores de coordenadas):, False caso contrário."""
+    return paramTADptoA[0] == paramTADptoB[0] and paramTADptoA[1] == paramTADptoB[1]
 # fim função
 
-def move(paramTADpto, float_dx, float_dy):
+def move(paramTADpto, dx, dy):
     """Desloca, no plano, o ponto de dx na coordenada x e de dy na
     coordenada y."""
+    return (paramTADpto[0] + dx, paramTADpto[1] + dy)
 # fim função
 
 def intersec(paramTADretA, paramTADretB):
     """Retorna um tad retângulo que é a intersecção dos dois retângulo de
     entrada, ou None se os retângulo de entrada não se intercederem."""
+    """if ():
+
+    elif ():
+
+    else:"""
+
+    # fim else
 # fim função
