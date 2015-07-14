f6d791b99d98f5d63f41456a62b5ac027b70d8b2
douglasbolis <douglas.lima@xdevel.com.br>
2015-07-14 15:40:39 -0300

    Finalizado função que verifica se um numero é natural.

--- recursividade/recursividade.py
+++ recursividade/recursividade.py
@@ -15,27 +15,6 @@ def fatorial(n):
     return factIter(1, n)
 # fim fatorial
 
-# (EXTRA_2)_Calcular a raiz quadrada de n
-def sqrt(n, stop):
-    def sqrtIter(aux, x):
-        if ehBomSuficiente(aux, x):
-            return aux
-        else:
-            return sqrtIter(melhoraResult(aux, x), x)
-            # fim else
-    # fim sqrtIter
-
-    def melhoraResult(param_aux, param_x):
-        return (param_aux + param_x / param_aux) / 2
-    # fim melhoraResult
-
-    def ehBomSuficiente(param_aux, param_x):
-        return abs((param_aux**2) - param_x) < stop
-    # fim ehBomSuficiente
-
-    return sqrtIter(1.0, n)
-# fim fatorial
-
 '''     Atividades da Lista     '''
 
 # (1)_Calcular a soma dos elementos de uma lista numérica.
@@ -60,6 +39,27 @@ def permutacao(lst):
     return permConj((), lst)
 # fim permutacao
 
+# (5)_Calcula a raiz quadrada de um número n com tolerância máxima t.
+def sqrt(n, t):
+    def sqrtIter(aux, x):
+        if ehBomSuficiente(aux, x):
+            return aux
+        else:
+            return sqrtIter(melhoraResult(aux, x), x)
+            # fim else
+    # fim sqrtIter
+
+    def melhoraResult(param_aux, param_x):
+        return (param_aux + param_x / param_aux) / 2
+    # fim melhoraResult
+
+    def ehBomSuficiente(param_aux, param_x):
+        return abs((param_aux**2) - param_x) < t
+    # fim ehBomSuficiente
+
+    return sqrtIter(1.0, n)
+# fim fatorial
+
 # (7)_Inverter uma string de entrada.
 def inverteStr(str):
     def invStr(novaStr, xstr):
@@ -71,6 +71,24 @@ def inverteStr(str):
 
     return invStr('', str)
 # fim inverteStr
+
+# (8)_Testar se um número n passado como parâmetro é um número natural.
+def ehNatural(n, incZero):
+    def verificaNum(param_n, param_limInf):
+        if param_n < param_limInf:
+            return False
+        elif param_n == param_limInf:
+            return True
+        else:
+            return verificaNum(param_n-1, param_limInf)
+    # verificaNum
+
+    if incZero:
+        return verificaNum(n, 0)
+    else:
+        return verificaNum(n, 1)
+    # fim else
+# fim ehNatural
 
 # (9)_Calcular o maior valor de uma lista de números fornecida como entrada.
 def maiorElem(lst):
@@ -84,7 +102,7 @@ def maiorElem(lst):
         # fim else
     # fim encontraMaiorElem
 
-    return encontraMaiorElem(0, lst)
+    return encontraMaiorElem(-1*fatorial(100), lst)
 # fim maiorElem
 
 # (10)_Calcular o menor valor de uma lista de números fornecida como entrad
@@ -146,20 +164,28 @@ def main():
     # Extra_1
     # print(fatorial(100))
 
-    # Extra_2
-    print(sqrt(23, 0.0001))
-
     # Exercício_1
     # lst = [12, 23, 34, 45, 56, 67, 78, 89, 90]
     # print(somaElem(lst))
+
+    # Exercício_5
+    # print(sqrt(4, 0.0001))
 
     # Exercício_7
     # str = 'socorram-me, subi no onibus em marrocos'
     # print(inverteStr(str))
 
+    # Exercício_8
+    # print(ehNatural(7, False))
+    # print(ehNatural(0, True))
+    # print(ehNatural(0, False))
+    # print(ehNatural(-8, False))
+    # print(ehNatural(2.5, False))
+
     # Exercício_9
-    # lst = [12, 23, 34, 45, 546, 67, 8]
-    # print(maiorElem(lst))
+    lst = [12, 23, 34, 45, 546, 67, 8]
+    lst2 = []
+    print(maiorElem(lst2))
 
     # Exercício_10
     # lst = [12, 23, 34, 45, 546, 67, 8]
