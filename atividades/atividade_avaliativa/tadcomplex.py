9c04115775654e123e08b316de6b8456e87fb49c
douglasbolis <douglas.lima@xdevel.com.br>
2015-07-19 13:46:25 -0300

    Atividade avaliativa incompleta.

--- atividades/atividade_avaliativa/tadcomplex.py
+++ atividades/atividade_avaliativa/tadcomplex.py
@@ -4,7 +4,9 @@ import math
 
 # A função "criar(<float real>,<float img>)" cria e retorna um TAD Complexo a partir dos valores de
 # entrada representando a parte real e a parte imaginária do número complexo.
-# EX.:  criar(2.0, 3.0)  -->  {"real": 2.0, "imaginario": 3.0}
+# EX.:
+# 	criar(2.0, 3.0)
+# 	{"real": 2.0, "imaginario": 3.0}
 #
 def criar(r, i):
 	return {"real": r, "imaginario": i}
@@ -13,7 +15,10 @@ def criar(r, i):
 # A função "criarP(<float modulo>,<float ang>)" cria e retorna um TAD Complexo a partir dos valores
 # de entrada representando o módulo e o ângulo do número complexo (forma polar).
 # Internamente deve ser convertido para a forma retangular.
-# EX.:  criaP(3.6056, 56.310)  -->  criar(2.0, 3.0) --> {"real": 2.0, "imaginario": 3.0}
+# EX.:
+# 	criaP(3.6056, 56.310)
+# 	criar(2.0, 3.0)
+# 	{"real": 2.0, "imaginario": 3.0}
 #
 def criaP(m, a):
 	return criar(m * math.cos(math.radians(a)), m * math.sin(math.radians(a)))
@@ -22,7 +27,10 @@ def criaP(m, a):
 # A função "criaStr(<string>)" cria e retorna um TAD Complexo a partir de uma string de entrada com
 # conteúdo sempre na forma 'n+mi', onde n e m são valores reais quaisquer representando a
 # parte real e a parte imaginaria do número complexo.
-# EX.:  criaStr('4.5+5.25i')  -->  cria(4.5, 5.25)  -->  {"real": 4.5, "imaginario": 5.25}
+# EX.:
+# 	criaStr('4.5+5.25i')
+# 	criar(4.5, 5.25)
+# 	{"real": 4.5, "imaginario": 5.25}
 #
 def criaStr(strComplexo):
 	if (strComplexo.find('+') > 0):
@@ -38,31 +46,70 @@ def criaStr(strComplexo):
 	#  fim else
 # fim criaStr
 
-
-#############	TERMINAR DAQUI	####################
+# A função "soma(<tadComplexA>,<tadComplexB>)" retorna um número complexo (TAD Complexo)
+# que é a soma dos números complexos de entrada.
+# EX.:
+# 	soma({"real": 6, "imaginario": 5}, {"real": 2, "imaginario": -1})
+# 	criar(6 + 2, 5 + (-1))
+# 	{"real": 8, "imaginario": 4}
+#
 def soma(numCpxA, numCpxB):
 	return criar(numCpxA["real"] + numCpxB["real"], numCpxA["imaginario"] + numCpxB["imaginario"])
 # fim soma
 
+# A função "multip(<tadComplexA>,<tadComplexB>)" retorna um número complexo (TAD Complexo)
+# que é o produto dos números complexos de entrada.
+# EX.:
+# 	multi({"real": 5, "imaginario": 1}, {"real": 2, "imaginario": -1})
+# 	criar((5*2 - 1*(-1), 5*(-1) + 1*2)
+# 	{"real": 11, "imaginario": -3}
+#
 def multi(numCpxA, numCpxB):
-	return [numCpxA[0] * numCpxB[0], numCpxA[1] * numCpxB[1]]
+	return  criar(
+		numCpxA["real"] * numCpxB["real"] - numCpxA["imaginario"] * numCpxB["imaginario"],
+		numCpxA["real"] * numCpxB["imaginario"] + numCpxA["imaginario"] * numCpxB["real"]
+	)
 # fim multi
 
+# A função "divid(<tadComplexA>,<tadComplexB>)" retorna um número complexo (TAD Complexo)
+# que é a divisão dos números complexos de entrada
+# EX.:
+# 	divid({"real": 5, "imaginario": 1}, {"real": 2, "imaginario": -1})
+#	criar((5*2 + 1*(-1)) / (2² + (-1)²), (2*1 - 5*(-1)) / (2² + (-1)²))
+#	{"real": 1.8, "imaginario": 1.4}
+#
 def divid(numCpxA, numCpxB):
-	return [numCpxA[0] / numCpxB[0], numCpxA[1] / numCpxB[1]]
+	return criar(
+		(numCpxA["real"] * numCpxB["real"] + numCpxA["imaginario"] * numCpxB["imaginario"]) / (numCpxB["real"]**2 + numCpxB["imaginario"]**2),
+		(numCpxB["real"] * numCpxA["imaginario"] - numCpxA["real"] * numCpxB["imaginario"]) / (numCpxB["real"]**2 + numCpxB["imaginario"]**2)
+	)
 # fim divid
 
+# A função "vezesK(<tadComplexA>,<k float>)" retorna um número complexo (TAD Complexo) cujos
+# componentes foram multiplicados por um valor qualquer float k.
+# EX.:
+# 	vezesK({"real": 1.8, "imaginario": 1.4}, 10)
+# 	criar(18, 14)
+# 	{"real": 18, "imaginario": 14}
+#
 def vezesK(numCpxA, k):
-	return [numCpxA[0] * k, numCpxA[1] * k]
+	return criar(numCpxA["real"] * k, numCpxA["imaginario"] * k)
 # fim funcao
 
+# A função "getStr(<tadComplexA>)" retorna o número complexo como uma string com o seguinte
+# formato: “r+mi”, sem espaços, onde r e m são números reais (1 casa decimal apenas)
+# representando as partes reais e imaginárias do número complexo.
+# EX.:
+# 	getStr({"real": 1.8, "imaginario": 1.4})
+#	“1.8+1.4i”
+#
 def getStr(numCpx):
-	strComplexo = str(numCpx[0])
+	strComplexo = "%.1f" %(numCpx["real"])
 
-	if (int(numCpx[1]) > 0):
-		strComplexo += '+' + str(numCpx[1][:-1] + 'i')
-	elif (int(numCpx[1]) < 0):
-		strComplexo += '-' + str((-1)*int(numCpx[1][:-1]) + 'i')
+	if (float(numCpx["imaginario"]) >= 0):
+		strComplexo += "+%.1fi" %(numCpx["imaginario"])
+	elif (float(numCpx["imaginario"]) < 0):
+		strComplexo += "%.1fi" %(numCpx["imaginario"])
 	# fim else
 
 	return strComplexo
--- atividades/atividade_avaliativa/tadmatrix.py
+++ atividades/atividade_avaliativa/tadmatrix.py
@@ -2,47 +2,62 @@ __author__ = 'Douglas'
 
 import tadcomplex
 
+# A função "criar(<altura int>,<largura int>)" cria e retorna um TMC de altura e largura definida pelos
+# parâmetros de entrada. O conteúdo de uma matriz recém-criada são elementos iguais ao
+# valor zero complexo (0 + 0i).
+#
 def criar(altA, altB):
-	lstMatrix = []
-	
+	dicMatrix = {}
+
+	dicMatrix["lin"] = altA
+	dicMatrix["col"] = altB
+
 	for i in range(altA):
-		lstAuxMatrix = []
 		for j in range(altB):
-			lstAuxMatrix.append('0+0i')
+			dicMatrix["%d%d" %(i+1, j+1)] = '0+0i'
 		# fim for
-		lstMatrix.append(lstAuxMatrix)
 	# fim for
 	
-	return lstMatrix	
-# fim funcao
+	return dicMatrix
+# fim criar
 
+# A função "adElem(<paramTMC>,<linha int>,<coluna int>,<tadComplex>)" altera a TMC de entrada
+# para que seu elemento de coordenadas linha, coluna armazene o valor de número complexo
+# igual ao parâmetro tadComplex
+#
 def adElem(paramTMC,linha,coluna,tadComplex):
+	paramTMC["%d%d" %(linha, coluna)] = tadcomplex.getStr(tadComplex)
+# fim adElem
 
-	paramTMC[linha][coluna] = tadcomplex.getStr(tadComplex)
-
-# fim funcao
-
+# A função "getElem(<paramTMC>,<linha int>,<coluna int>)" retorna o elemento TAD Complexo
+# residente na posição linha, coluna do parâmetro TMC de entrada.
+#
 def getElem(paramTMC,linha,coluna):
-	return paramTMC[linha][coluna]
-# fim funcao
+	return tadcomplex.criaStr(paramTMC["%d%d" %(linha, coluna)])
+# fim getElem
 
+# A função "getLinhasTMC(<paramTMC>)" retorna a quantidade de linhas da TMC
+# parâmetro de entrada.
+#
 def getLinhasTMC(paramTMC):
-	return len(paramTMC)
-# fim funcao
+	return paramTMC["lin"]
+# fim getLinhasTMC
 
+# A função "getColunasTMC(<paramTMC>)" retorna a quantidade de colunas da TMC
+# parâmetro de entrada
+#
 def getColunasTMC(paramTMC):
-	return len(paramTMC[0])
-# fim funcao
+	return paramTMC["col"]
+# fim getColunasTMC
 
-def multiTMC(paramTMCa, paramTMCb):
-	novaTadMatrix = []
-
-	for i in range(len(paramTMCa)):
-		lstAuxTadMatrix = []
-		for j in range(len(paramTMCa[i])):
-			lstAuxTadMatrix.append(paramTMCa[i][j]*paramTMCb[j][i])
+# A função "imprimeTMC(<paramTMC>)" faz a impressão organizada na forma matricial da TMC
+# passada como parâmetro de entrada.
+#
+def imprimeTMC(paramTMC):
+	for i in range(paramTMC["lin"]):
+		for j in range(paramTMC["col"]):
+			print(paramTMC["%d%d" %(i+1, j+1)] + "	", end="")
 		# fim for
-		novaTadMatrix.append(lstAuxTadMatrix)
+		print()
 	# fim for
-	return novaTadMatrix
-# fim funcao
+# fim imprimeTMC
